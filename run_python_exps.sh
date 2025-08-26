89b54ddf4e5996ff9a1d856985df6a173857ff7b
aledcuevas <aledancuevas@gmail.com>
2025-08-26 16:57:46 -0300

    second commit

--- run_python_exps.sh
+++ run_python_exps.sh
@@ -9,6 +9,9 @@ python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 
+# ensure output directory exists
+mkdir -p output
+
 # 2) Install deps
 pip install -r requirements.txt
 
